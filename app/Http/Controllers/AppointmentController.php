c0c14e68e1181f9ee4b9b6b2b50460a6ba14fc9e
Alan Santos <alanssantos32@gmail.com>
2021-11-15 02:06:26 -0300

    Update appointment

--- app/Http/Controllers/AppointmentController.php
+++ app/Http/Controllers/AppointmentController.php
@@ -2,69 +2,38 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Database\QueryException;
-use DB;
-use Carbon\Carbon;
 use Illuminate\Http\Request;
 use App\Models\Appointment;
-use App\Models\Medic;
 use App\Models\Receptionist;
 use App\Models\Patient;
-use App\Models\Performs;
 
 class AppointmentController extends Controller
 {
     public function index() {
         $recepcionistas = Receptionist::select('cpf','name as nameRec');
-
-        $medicos = Medic::select('cpf as cpfMed', 'name as nameMed');
         $pacientes = Patient::select('cpf as cpfPac', 'name as namePac');
         
-        $performs = Performs::joinSub($medicos, 'medicos', function ($join) {
-            $join->on('medicos.cpfMed', '=', 'performs.cpfMed');
-        });
-
         $appointments = Appointment::joinSub($pacientes, 'pacientes', function ($join) {
             $join->on('appointment.cpfPac', '=', 'pacientes.cpfPac');
-        })->joinSub($performs, 'performs', function ($join) {
-            $join->on('appointment.id', '=', 'performs.idAppointment');
+        })->joinSub($recepcionistas, 'recepcionistas', function ($join) {
+            $join->on('appointment.cpfRec', '=', 'recepcionistas.cpf');
         });
 
         $data['appointments'] = $appointments->orderBy('appointment.date', 'DESC')->get();
-
-        $data['recepcionistas'] = $recepcionistas->get();
-        $data['medicos'] = $medicos->get();
+        $data['receptionists'] = $recepcionistas->get();
+        $data['patients'] = $pacientes->get();
 
         return view('appointment', $data);
     }
 
     public function store(Request $request) {
-        
-        $appointment_request = [
-            'cpfRec' => $request->input('cpfRec'),
-            'cpfPac' => $request->input('cpfPac'),
-            'date' => Carbon::now()
-        ];
-
-        try {
-            $Appointment = Appointment::create($appointment_request);
-        } catch (QueryException $ex) {
-            return redirect('/appointment')->withErrors(["cpfPac" => "Paciente inválido."]);
-        }
-
-        $performs_request = [
-            'idAppointment' => $Appointment->id,
-            'cpfMed' => $request->input('cpfMed'),
-            'date' => $request->input('data') . ' ' . $request->input('hora')
-        ];
-
-        Performs::create($performs_request);
+        date_default_timezone_set('America/Sao_Paulo');
+        Appointment::create($request->all() + ['date' => date("Y-m-d H:i:s")]);
 
         return redirect('/appointment');
     }
 
     public function delete($id) {
-        Performs::where('idAppointment', $id)->delete();
         Appointment::where('id', $id)->delete();
         return redirect('/appointment');
     }
--- resources/views/appointment.blade.php
+++ resources/views/appointment.blade.php
@@ -1,15 +1,15 @@
 @extends('layout')
 <?php 
-        function converteData($dataHora) {
-            $p = explode(' ', $dataHora);
-            $data = $p[0];
-            $hora = $p[1];
-            $hora = substr($hora,0,5);
+    function converteData($dataHora) {
+        $p = explode(' ', $dataHora);
+        $data = $p[0];
+        $hora = $p[1];
+        $hora = substr($hora,0,5);
 
-            $p = explode('-', $data);
-            $data = $p[2] . '/' . $p[1] . '/' . $p[0];
-            return $data . ' ' . $hora;
-        }
+        $p = explode('-', $data);
+        $data = $p[2] . '/' . $p[1] . '/' . $p[0];
+        return $data . ' ' . $hora;
+    }
 ?>
 @section('content')
     <h1 class="mt-5 mb-5">Lista de consultas</h1>
@@ -27,23 +27,11 @@
                     <form method="POST" action="/appointment" class="row g-3 needs-validation" novalidate>
                         {{  csrf_field() }}
                         <div class="col-md-6">
-                            <label for="validationCustom03" class="form-label">Recepcionista Responsável</label>
-                            <select name="cpfRec" id="validationCustom03" class="form-select form-control" required>
-                                @foreach ($recepcionistas as $recepcionista)
-                                    <option value="{{ $recepcionista->cpf }}"> {{ $recepcionista->nameRec }}</option>
-                                @endforeach
-                            </select>
-                            <div class="invalid-feedback">
-                                É necessário preencher o campo corretamente!
-                            </div>
-                        </div>
-
-
-                        <div class="col-md-6">
-                            <label for="validationCustom03" class="form-label">Médico</label>
-                            <select name="cpfMed" id="validationCustom03" class="form-select form-control" required>
-                                @foreach ($medicos as $medico)
-                                    <option value="{{ $medico->cpfMed }}"> {{ $medico->nameMed }}</option>
+                            <label for="validationCustom01" class="form-label">Recepcionista Responsável</label>
+                            <select name="cpfRec" id="validationCustom01" class="form-select" required>
+                                <option value="">Selecione um recepcionista</option>
+                                @foreach ($receptionists as $receptionist)
+                                    <option value="{{ $receptionist->cpf }}"> {{ $receptionist->nameRec }}</option>
                                 @endforeach
                             </select>
                             <div class="invalid-feedback">
@@ -52,24 +40,13 @@
                         </div>
 
                         <div class="col-md-6">
-                            <label for="validationCustom03" class="form-label">CPF Paciente</label>
-                            <input type="text" name="cpfPac" class="form-control cpf-mask" id="validationCustom03" maxlength="14" placeholder="XXX.XXX.XXX-XX" required>
-                            <div class="invalid-feedback">
-                                É necessário preencher o campo corretamente!
-                            </div>
-                        </div>
-
-                        <div class="col-md-6">
-                            <label for="validationCustom03" class="form-label">Data</label>
-                            <input type="date" name="data" class="form-control" id="validationCustom03" placeholder="__/__/____" maxlength="10" required>
-                            <div class="invalid-feedback">
-                                É necessário preencher o campo corretamente!
-                            </div>
-                        </div>
-
-                        <div class="col-md-6">
-                            <label for="validationCustom03" class="form-label">Hora</label>
-                            <input type="text" name="hora" class="form-control time-mask" id="validationCustom03" maxlength="5" placeholder="__:__" required>
+                            <label for="validationCustom02" class="form-label">Recepcionista Responsável</label>
+                            <select name="cpfPac" id="validationCustom02" class="form-select" required>
+                                <option value="">Selecione um paciente</option>
+                                @foreach ($patients as $patient)
+                                    <option value="{{ $patient->cpfPac }}"> {{ $patient->namePac }}</option>
+                                @endforeach
+                            </select>
                             <div class="invalid-feedback">
                                 É necessário preencher o campo corretamente!
                             </div>
@@ -98,9 +75,9 @@
         <thead>
             <tr>
                 <th scope="col">#</th>
-                <th scope="col">Data</th>
+                <th scope="col">Recepcionista</th>
                 <th scope="col">Paciente</th>
-                <th scope="col">Médico</th>
+                <th scope="col">Data</th>
                 <th></th>
             </tr>
         </thead>
@@ -109,9 +86,9 @@
             @for ($i = 0; $i < count($appointments); $i++)
                 <tr class="align-middle">
                     <th scope="row">{{ $i + 1 }}</th>
-                    <td>{{ converteData($appointments[$i]->date) }}</td>
+                    <td>{{ $appointments[$i]->nameRec }}</td>
                     <td>{{ $appointments[$i]->namePac }}</td>
-                    <td>{{ $appointments[$i]->nameMed }}</td>
+                    <td>{{ converteData($appointments[$i]->date) }}</td>
                     <td> 
                         <form method="post" class="delete_form" action="/appointment/{{ $appointments[$i]->id }}">
                             {{ method_field('DELETE') }}
@@ -130,30 +107,25 @@
         </tbody>
     </table>
 
-
 <script>
-    function formatCPF(cpf){
-        if (cpf.length > 14) cpf = cpf.slice(0, 14);
-        cpf = cpf.replace(/[^\d]/g, "");
-        return cpf.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, "$1.$2.$3-$4");
-    }
+    (function () {
+    'use strict'
 
-    let input = document.querySelector('.cpf-mask');
-    input.addEventListener('keyup', e => {
-        let res = formatCPF(e.target.value);
-        e.target.value = res;
-    });
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation')
 
-    function formatTime(t){
-        if (t.length > 5) t = t.slice(0, 5);
-        t = t.replace(/[^\d]/g, "");
-        return t.replace(/(\d{2})(\d{2})/, "$1:$2");
-    }
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+        .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+            if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+            }
 
-    input = document.querySelector('.time-mask');
-    input.addEventListener('keyup', e => {
-        let res = formatTime(e.target.value);
-        e.target.value = res;
-    });
+            form.classList.add('was-validated')
+        }, false)
+        })
+    })();
 </script>
 @endsection
