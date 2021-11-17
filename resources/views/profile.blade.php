ffbc3e3584d9b2790eb7bff5be49889b937ebe54
Franco <franconoronha11@gmail.com>
2021-11-17 14:15:41 -0300

    profile css

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -11,6 +11,24 @@
         <script src="{{ asset('js/jquery.js') }}"></script>
         <script src="{{ asset('js/bootstrap.js') }}"></script>
     </head>
+    <style>
+        body {
+            padding: 10px;
+        }
+        #titulo {
+            text-align: center;
+        }
+        #div-profile {
+            display: flex;
+        }
+        #div-profile > div {
+            width: 50%;
+        }
+        .interna {
+            padding: 10px;
+            outline: 1px solid black;
+        }
+    </style>
     <body>
         <?php 
         function converteData($dataHora) {
@@ -26,23 +44,23 @@
         ?>
         <div class="center">
             <div>
-                <h2>Olá {{ $patient->name }}</h2>
+                <h2 id="titulo">Olá {{ $patient->name }}</h2>
             </div>
-            <div>
-                <div>
+            <div id="div-profile">
+                <div class="container">
                     <h1> Consultas </h1>
                     @foreach ($appointments as $app)
-                        <div>
+                        <div class="interna">
                         <p>Consulta {{ converteData($app->datePerforms) }} </p>
                         <p>Médico: {{ $app->nameMed }}</p>
                         <p>Diagnóstico: {{ $app->diagnosis }}</p>
                         </div>
                     @endforeach
                 </div>
-                <div>
+                <div class="container">
                     <h1> Exames </h1>
                     @foreach ($takeExams as $exam)
-                    <div>
+                    <div class="interna">
                     <p>Exame {{ converteData($exam->dateExam) }} </p>
                     <p>Tipo: {{ $exam->nameExam}}</p>
                     <p>Enfermeiro: {{ $exam->nameNurse}}</p>
